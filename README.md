0da330d08ede49093f38b18dd052b09c8bc9d564
Gift Ngerem <vas@broadbased.net>
2026-06-11 15:56:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ivytech_website
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: { //  Correct property name
+    rollupOptions: {
       external: ['some-package'],
     }
   }
